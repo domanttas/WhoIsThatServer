21340d0855c93eb3bb738d7b028aaaa1c0f1d3cc
Dom <domantasgiedraitis@gmail.com>
2018-10-01 19:40:25 +0300

    Added DatabaseImageElementHelper

---
+++ WhoIsThatServer.Storage/Helpers/DatabaseImageElementHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.Helpers
+{
+    public class DatabaseImageElementHelper : IDatabaseImageElementHelper
+    {
+        public DatabaseImageElement InsertNewImageElement(string ImageName, string ImageContentUri, string PersonFirstName, string PersonLastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DatabaseImageElement> GetAllImages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
