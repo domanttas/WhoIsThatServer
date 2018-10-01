ae34a63bbe446c05882d31d653e2f44274a00265
Dom <domantasgiedraitis@gmail.com>
2018-10-01 19:56:11 +0300

    Added DatabaseContext and generation for connection to DB

---
+++ WhoIsThatServer.Storage/Context/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        /// <summary>
+        /// Generates database called WhoIsThatServer.Storage.DatabaseContext
+        /// Generates Tables according to their names from properties of type DbSet<T>.
+        /// Used to access DB information.
+        /// </summary>
+        public DatabaseContext() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Stores information about database image element
+        /// </summary>
+        public virtual DbSet<DatabaseImageElement> DatabaseImageElement { get; set; }
+
+
+    }
+}
