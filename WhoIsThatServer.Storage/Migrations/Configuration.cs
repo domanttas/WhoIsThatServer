99d67da2882a03d9137a8b281c0bea9c5fccab67
Dom <domantasgiedraitis@gmail.com>
2018-10-03 15:27:31 +0300

    Added migrations configuration file

---
+++ WhoIsThatServer.Storage/Migrations/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace WhoIsThatServer.Storage.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<WhoIsThatServer.Storage.Context.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "WhoIsThatServer.Storage.Context.DatabaseContext";
+        }
+    }
+}
