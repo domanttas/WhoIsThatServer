ab64b5bfb836dd7664f163641b063ba2a51e9d26
Dom <domantasgiedraitis@gmail.com>
2018-10-03 15:22:54 +0300

    Added DatabaseImageElementController

---
+++ WhoIsThatServer.Storage/Controllers/DatabaseImageElementController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WhoIsThatServer.Storage.Context;
+using WhoIsThatServer.Storage.Helpers;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.Controllers
+{
+    public class DatabaseImageElementController : ApiController
+    {
+        public IDatabaseImageElementHelper DatabaseImageElementHelper { get; set; } = new DatabaseImageElementHelper();
+        public DatabaseContextGeneration DatabaseContextGeneration { get; set; } = new DatabaseContextGeneration();
+
+        [HttpGet]
+        public IHttpActionResult GetAllImages()
+        {
+            return Json(DatabaseImageElementHelper.GetAllImages());
+        }
+
+        public IHttpActionResult Post([FromBody] DatabaseImageElement databaseImageElement)
+        {
+            using (var context = DatabaseContextGeneration.BuildDatabaseContext())
+            {
+                context.DatabaseImageElements.Add(databaseImageElement);
+                context.SaveChanges();
+            }
+
+            return Json(databaseImageElement);
+        }
+    }
+}
