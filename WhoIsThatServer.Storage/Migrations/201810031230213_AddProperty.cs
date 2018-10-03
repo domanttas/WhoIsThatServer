00ba66b95f86677f37a08ae87626ff77dab32d46
Dom <domantasgiedraitis@gmail.com>
2018-10-03 15:31:50 +0300

    Added migrations property

---
+++ WhoIsThatServer.Storage/Migrations/201810031230213_AddProperty.cs
@@ -0,0 +1,29 @@
+namespace WhoIsThatServer.Storage.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProperty : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.DatabaseImageElements",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ImageName = c.String(),
+                        ImageContentUri = c.String(),
+                        PersonFirstName = c.String(),
+                        PersonLastName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.DatabaseImageElements");
+        }
+    }
+}
