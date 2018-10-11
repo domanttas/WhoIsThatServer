bff61fbc70a51583e4bcc9709823a4ae73ae2b49
Dom <domantasgiedraitis@gmail.com>
2018-10-11 15:29:21 +0300

    Added unit test for controller method Post, fixed controller according to test

--- WhoIsThatServer.Storage/UnitTests/DatabaseImageElementControllerTests.cs
+++ WhoIsThatServer.Storage/UnitTests/DatabaseImageElementControllerTests.cs
@@ -54,5 +54,52 @@ namespace WhoIsThatServer.Storage.UnitTests
             
             jsonContent.ShouldBe(expectedJson);
         }
+
+        [Test]
+        public async Task Post_ShouldCallHelper()
+        {
+            //Arrange
+            var expectedId = 1;
+            var expectedImageName = "testImageName";
+            var expectedImageContentUri = "testURI";
+            var expectedPersonFirstName = "testFirstName";
+            var expectedPersonLastName = "testLastName";
+
+            var expectedDatabaseImageElement = new DatabaseImageElement()
+            {
+                Id = expectedId,
+                ImageContentUri = expectedImageContentUri,
+                ImageName = expectedImageName,
+                PersonFirstName = expectedPersonFirstName,
+                PersonLastName = expectedPersonLastName
+            };
+            
+            var fakeDatabaseImageElementHelper = A.Fake<IDatabaseImageElementHelper>();
+
+            A.CallTo(() => fakeDatabaseImageElementHelper.InsertNewImageElement(expectedId, expectedImageName,
+                    expectedImageContentUri, expectedPersonFirstName, expectedPersonLastName))
+                .Returns(expectedDatabaseImageElement);
+            
+            var databaseImageElementController = new DatabaseImageElementController()
+            {
+                DatabaseImageElementHelper = fakeDatabaseImageElementHelper,
+                Request = new HttpRequestMessage()
+            };
+            
+            //Act
+            var result = databaseImageElementController.Post(expectedDatabaseImageElement);
+            
+            //Assert
+            A.CallTo(() => fakeDatabaseImageElementHelper.InsertNewImageElement(expectedId, expectedImageName,
+                    expectedImageContentUri, expectedPersonFirstName, expectedPersonLastName))
+                .MustHaveHappenedOnceExactly();
+            
+            var httpResponse = await result.ExecuteAsync(new CancellationToken());
+            var jsonContent = await httpResponse.Content.ReadAsStringAsync();
+
+            var expectedJson = JsonConvert.SerializeObject(expectedDatabaseImageElement);
+            
+            jsonContent.ShouldBe(expectedJson);
+        }
     }
 }
