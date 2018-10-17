8be04a6c0ae1e8c4e2f7ac7ee38228d871a83729
Dom <domantasgiedraitis@gmail.com>
2018-10-17 11:49:00 +0300

    Added helper for azure cloud storage

---
+++ WhoIsThatServer.Recognition/Helpers/AzureBlobHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.Helpers
+{
+    public class AzureBlobHelper
+    {
+        public AzureBlob AzureBlob;
+
+        public AzureBlobHelper()
+        {
+            AzureBlob = new AzureBlob();
+        }
+
+        public async void SetPermissionsToPublic()
+        {
+            BlobContainerPermissions permissions = new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            };
+            await AzureBlob.CloudBlobContainer.SetPermissionsAsync(permissions);
+        }
+
+        public string GetImageUri(string imageName)
+        {
+            AzureBlob.CloudBlockBlob = AzureBlob.CloudBlobContainer.GetBlockBlobReference(imageName);
+            return AzureBlob.CloudBlockBlob.Uri.ToString();
+        }
+
+        public async void DeletePhoto(string name)
+        {
+            var blob = AzureBlob.CloudBlobContainer.GetBlockBlobReference(name);
+            await blob.DeleteIfExistsAsync();
+        }
+    }
+}
