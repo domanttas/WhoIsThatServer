6a5d5f4ef87ff31ff211776159bab62347c09458
Dom <domantasgiedraitis@gmail.com>
2018-10-17 11:47:52 +0300

    Added model for azure cloud storage

---
+++ WhoIsThatServer.Recognition/Models/AzureBlob.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhoIsThatServer.Storage.Models
+{
+    public class AzureBlob
+    {
+        public string StorageConnectionString { get; set; }
+        public CloudStorageAccount StorageAccount { get; set; }
+        public CloudBlobContainer CloudBlobContainer { get; set; }
+        public CloudBlobClient CloudBlobClient { get; set; }
+        public CloudBlockBlob CloudBlockBlob { get; set; }
+
+        public AzureBlob()
+        {
+            //Getting storage string to Azure Storage
+            StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=whoisthatserverimages;AccountKey=7nJ73XYUJyGrNKs6MYQRnfD80AnkmAOmgvBMc7WSqiVGM0wF2HntAZNPDNctmo44yoQ+iIjVqVjucLMMiCDv+w==";
+
+            //Parse storage account
+            StorageAccount = CloudStorageAccount.Parse(StorageConnectionString);
+
+            //Create the CloudBlobClient that represents the Blob storage endpoint for the storage account
+            CloudBlobClient = StorageAccount.CreateCloudBlobClient();
+
+            //Get container's 'images' reference
+            CloudBlobContainer = CloudBlobClient.GetContainerReference("images");
+        }
+    }
+}
