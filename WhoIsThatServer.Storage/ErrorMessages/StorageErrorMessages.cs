3a25edb09dbdc0386b8aacace2e8aab7f7b9de0b
Dom <domantasgiedraitis@gmail.com>
2018-10-28 13:03:42 +0200

    Created error message class in storage

---
+++ WhoIsThatServer.Storage/ErrorMessages/StorageErrorMessages.cs
@@ -0,0 +1,7 @@
+namespace WhoIsThatServer.Storage.ErrorMessages
+{
+    public static class StorageErrorMessages
+    {
+        public const string TargetNotFoundError = "This is not your target";
+    }
+}
