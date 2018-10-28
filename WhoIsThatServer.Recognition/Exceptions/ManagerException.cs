7e77371e27a7ccf0bdfad85152cd1f4e751bcb5e
Dom <domantasgiedraitis@gmail.com>
2018-10-28 07:54:04 +0200

    Created custom exception class

---
+++ WhoIsThatServer.Recognition/Exceptions/ManagerException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhoIsThatServer.Recognition.Exceptions
+{
+    public class ManagerException : Exception
+    {
+        public string ErrorCode { get; set; }
+
+        public ManagerException() : base()
+        {
+        }
+
+        public ManagerException(string code) : base()
+        {
+            ErrorCode = code;
+        }
+    }
+}
