7f6c2297ca100c6ffab01050beab4536c80b57cd
Dom <domantasgiedraitis@gmail.com>
2018-10-31 16:41:23 +0200

    Added unit test for FaceFeaturesController's Post method

---
+++ WhoIsThatServer.Storage/UnitTests/FaceFeaturesControllerTests.cs
@@ -0,0 +1,60 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Shouldly;
+using WhoIsThatServer.Storage.Controllers;
+using WhoIsThatServer.Storage.Helpers;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.UnitTests
+{
+    public class FaceFeaturesControllerTests
+    {
+        [Test]
+        public async Task Post_ShouldCallHelper()
+        {
+            //Arrange
+            var expectedId = 0;
+            var expectedPersonId = 1;
+            var expectedAge = 20;
+            var expectedGender = "male";
+
+            var expectedFeaturesElement = new FaceFeaturesModel()
+            {
+                Id = expectedId,
+                PersonId = expectedPersonId,
+                Age = expectedAge,
+                Gender = expectedGender
+            };
+
+            var fakeFaceFeaturesHelper = A.Fake<IFaceFeaturesHelper>();
+
+            A.CallTo(() =>
+                    fakeFaceFeaturesHelper.InsertNewFeaturesModel(expectedPersonId, expectedAge, expectedGender))
+                .Returns(expectedFeaturesElement);
+
+            var faceFeaturesController = new FaceFeaturesController()
+            {
+                FaceFeaturesHelper = fakeFaceFeaturesHelper,
+                Request = new HttpRequestMessage()
+            };
+            
+            //Act
+            var result = faceFeaturesController.Post(expectedFeaturesElement);
+            
+            //Assert
+            A.CallTo(() => fakeFaceFeaturesHelper.InsertNewFeaturesModel(expectedPersonId, expectedAge, expectedGender))
+                .MustHaveHappenedOnceExactly();
+            
+            var httpResponse = await result.ExecuteAsync(new CancellationToken());
+            var jsonContent = await httpResponse.Content.ReadAsStringAsync();
+
+            var expectedJson = JsonConvert.SerializeObject(expectedFeaturesElement);
+            
+            jsonContent.ShouldBe(expectedJson);
+        }
+    }
+}
