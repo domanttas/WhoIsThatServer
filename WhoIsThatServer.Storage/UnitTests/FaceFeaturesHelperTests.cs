fed45a4a23a56d2050cbad0435b27bd68463cb7b
Dom <domantasgiedraitis@gmail.com>
2018-10-31 16:33:14 +0200

    Added unit test for FaceFeaturesHelper's method to get user by ID when should throw

--- WhoIsThatServer.Storage/UnitTests/FaceFeaturesHelperTests.cs
+++ WhoIsThatServer.Storage/UnitTests/FaceFeaturesHelperTests.cs
@@ -4,6 +4,8 @@ using FakeItEasy;
 using NUnit.Framework;
 using Shouldly;
 using WhoIsThatServer.Storage.Context;
+using WhoIsThatServer.Storage.ErrorMessages;
+using WhoIsThatServer.Storage.Exceptions;
 using WhoIsThatServer.Storage.Helpers;
 using WhoIsThatServer.Storage.Models;
 
@@ -85,6 +87,43 @@ namespace WhoIsThatServer.Storage.UnitTests
             result.Age.ShouldBe(expectedAge);
             result.Gender.ShouldBe(expectedGender);
             result.PersonId.ShouldBe(expectedPersonId);
+        }
+
+        [Test]
+        public void GetFaceFeaturesByPersonId_WhenIdDoesNotExists_ShouldThrow()
+        {
+            //Arrange
+            var expectedId = 0;
+            var expectedPersonId = 1;
+            var expectedAge = 20;
+            var expectedGender = "male";
+
+            var fakeIQueryable = new List<FaceFeaturesModel>()
+            {
+                new FaceFeaturesModel()
+                {
+                    Id = expectedId,
+                    PersonId = expectedPersonId,
+                    Age = expectedAge,
+                    Gender = expectedGender
+                }
+            }.AsQueryable();
+
+            var fakeDbSet = UnitTestsUtil.SetupFakeDbSet(fakeIQueryable);
+            
+            var fakeDbContext = A.Fake<DatabaseContext>();
+            A.CallTo(() => fakeDbContext.FaceFeatures).Returns(fakeDbSet);
+            
+            var fakeDbContextGeneration = A.Fake<IDatabaseContextGeneration>();
+            A.CallTo(() => fakeDbContextGeneration.BuildDatabaseContext()).Returns(fakeDbContext);
+            
+            var faceFeaturesHelper = new FaceFeaturesHelper(fakeDbContextGeneration);
+            
+            //Act and assert
+            Assert.Throws<ManagerException>(() => faceFeaturesHelper.GetFaceFeaturesByPersonId(100),
+                StorageErrorMessages.UserDoesNotExistError);
+
+            A.CallTo(() => fakeDbContextGeneration.BuildDatabaseContext()).MustHaveHappenedOnceExactly();
         }
     }
 }
