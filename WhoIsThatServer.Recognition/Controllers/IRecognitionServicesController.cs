034e81836a1fb806d3c4830f9615ee6907197d73
Dom <domantasgiedraitis@gmail.com>
2018-11-04 15:55:34 +0200

    Recognition now returns bad requests

--- WhoIsThatServer.Recognition/Controllers/IRecognitionServicesController.cs
+++ WhoIsThatServer.Recognition/Controllers/IRecognitionServicesController.cs
@@ -13,26 +13,26 @@ namespace WhoIsThatServer.Recognition.Controllers
         /// Calls method responsible for image recognition in RecognitionServices
         /// </summary>
         /// <returns>Name of detected person</returns>
-        Task<JsonResult> InitiateRecognition();
+        Task<ActionResult> InitiateRecognition();
 
         /// <summary>
         /// Inserts person into person group
         /// </summary>
         /// <param name="imageModel">Object of person from frontend</param>
         /// <returns>Boolean</returns>
-        Task<JsonResult> Post([FromBody] ImageModel imageModel);
+        Task<ActionResult> Post([FromBody] ImageModel imageModel);
 
         /// <summary>
         /// Creates person group
         /// </summary>
         /// <returns></returns>
-        Task<JsonResult> Create();
+        Task<ActionResult> Create();
 
         /// <summary>
         /// Detects features of face
         /// </summary>
         /// <param name="imageModel">Object of person from frontend</param>
         /// <returns>Detected features as array</returns>
-        Task<JsonResult> DetectFeaturesOfFace([FromBody] ImageModel imageModel);
+        Task<ActionResult> DetectFeaturesOfFace([FromBody] ImageModel imageModel);
     }
 }
