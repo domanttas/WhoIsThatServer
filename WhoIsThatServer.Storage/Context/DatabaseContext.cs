192e355f49550dc09bc1f26026081cb7d80876af
Dom <domantasgiedraitis@gmail.com>
2018-11-10 19:12:02 +0200

    Moved DB connection string to config file

--- WhoIsThatServer.Storage/Context/DatabaseContext.cs
+++ WhoIsThatServer.Storage/Context/DatabaseContext.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Microsoft.Azure;
 using WhoIsThatServer.Storage.Models;
 
 namespace WhoIsThatServer.Storage.Context
 {
     public class DatabaseContext : DbContext, IDatabaseContext
     {
-        /// <summary>
-        /// Generates database called WhoIsThatServer.Storage.DatabaseContext
-        /// Generates Tables according to their names from properties of type DbSet<T>.
-        /// Used to access DB information.
-        /// </summary>
-        public DatabaseContext() : base("Data Source=teststorageserverdbserver.database.windows.net;Initial Catalog=TestStorageServer_db;User ID=sqladmin;Password=Vs8rTr3k;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        /// <inheritdoc />
+        public DatabaseContext() : base(CloudConfigurationManager.GetSetting("DbConnectionString"))
         {
 
         }
