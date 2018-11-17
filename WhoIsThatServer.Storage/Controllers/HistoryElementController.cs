d6014cabfc1b489f55316324f52dc469784d848b
Dom <domantasgiedraitis@gmail.com>
2018-11-17 12:20:46 +0200

    History element controller's methods

---
+++ WhoIsThatServer.Storage/Controllers/HistoryElementController.cs
@@ -0,0 +1,38 @@
+using System.Web.Http;
+using WhoIsThatServer.Storage.ErrorMessages;
+using WhoIsThatServer.Storage.Exceptions;
+using WhoIsThatServer.Storage.Helpers;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.Controllers
+{
+    public class HistoryElementController : ApiController, IHistoryElementController
+    {
+        public IHistoryModelHelper HistoryModelHelper { get; set; } = new HistoryModelHelper();
+        
+        /// <inheritdoc/>
+        [HttpPost]
+        [Route("api/history")]
+        public IHttpActionResult Post([FromBody] HistoryModel historyModel)
+        {
+            return Json(HistoryModelHelper.InsertNewHistoryElement(historyModel.UserId, historyModel.TargetId, historyModel.Status));
+        }
+        
+        /// <inheritdoc/>
+        [HttpGet]
+        [Route("api/history/{userId}")]
+        public IHttpActionResult GetElementById(int userId)
+        {
+            try
+            {
+                return Json(HistoryModelHelper.GetHistoryByUserId(userId));
+            }
+
+            catch (ManagerException historyNotFoundException) when (historyNotFoundException.ErrorCode ==
+                                                                    StorageErrorMessages.HistoryElementNotFoundError)
+            {
+                return BadRequest(StorageErrorMessages.HistoryElementNotFoundError);
+            }
+        }
+    }
+}
---
+++ WhoIsThatServer.Storage/Controllers/IHistoryElementController.cs
@@ -0,0 +1,22 @@
+using System.Web.Http;
+using WhoIsThatServer.Storage.Models;
+
+namespace WhoIsThatServer.Storage.Controllers
+{
+    public interface IHistoryElementController
+    {
+        /// <summary>
+        /// Calls helper which posts history element into DB
+        /// </summary>
+        /// <param name="historyModel">Model to insert</param>
+        /// <returns>Inserted model</returns>
+        IHttpActionResult Post([FromBody] HistoryModel historyModel);
+
+        /// <summary>
+        /// Calls helper which gets history element by user ID from DB
+        /// </summary>
+        /// <param name="userId">User ID</param>
+        /// <returns>History model</returns>
+        IHttpActionResult GetElementById(int userId);
+    }
+}
