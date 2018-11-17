87e3a30debd6126126ebcfd1af07c40b05da7dd9
Dom <domantasgiedraitis@gmail.com>
2018-11-17 17:21:21 +0200

    History controller's unit tests to update status

--- WhoIsThatServer.Storage/UnitTests/HistoryElementControllerTests.cs
+++ WhoIsThatServer.Storage/UnitTests/HistoryElementControllerTests.cs
@@ -57,9 +57,8 @@ namespace WhoIsThatServer.Storage.UnitTests
         }
 
         [Test]
-        public async Task GetElementById()
+        public async Task GetElementById_ShouldCallHelper()
         {
-            //Arrange
             //Arrange
             var expectedUserId = 1;
             var expectedTargetId = 2;
@@ -86,6 +85,45 @@ namespace WhoIsThatServer.Storage.UnitTests
             
             //Assert
             A.CallTo(() => fakeHistoryModelHelper.GetHistoryByUserId(expectedUserId)).MustHaveHappened();
+            
+            var httpResponse = await result.ExecuteAsync(new CancellationToken());
+            var jsonContent = await httpResponse.Content.ReadAsStringAsync();
+            
+            var expectedJson = JsonConvert.SerializeObject(expectedElement);
+            
+            jsonContent.ShouldBe(expectedJson);
+        }
+
+        [Test]
+        public async Task UpdateHistoryElement_ShouldCallHelper()
+        {
+            //Arrange
+            var expectedUserId = 1;
+            var expectedTargetId = 2;
+            var expectedStatus = StatusConstants.TargetHuntedHistory;
+
+            var expectedElement = new HistoryModel()
+            {
+                Id = 50,
+                UserId = expectedUserId,
+                TargetId = expectedTargetId,
+                Status = expectedStatus
+            };
+            
+            var fakeHistoryModelHelper = A.Fake<IHistoryModelHelper>();
+            A.CallTo(() => fakeHistoryModelHelper.UpdateHistoryModel(expectedUserId)).Returns(expectedElement);
+            
+            var historyElementController = new HistoryElementController()
+            {
+                HistoryModelHelper = fakeHistoryModelHelper,
+                Request = new HttpRequestMessage()
+            };
+            
+            //Act
+            var result = historyElementController.UpdateHistoryElement(expectedUserId);
+            
+            //Assert
+            A.CallTo(() => fakeHistoryModelHelper.UpdateHistoryModel(expectedUserId)).MustHaveHappened();
             
             var httpResponse = await result.ExecuteAsync(new CancellationToken());
             var jsonContent = await httpResponse.Content.ReadAsStringAsync();
